ce51676c24f5f88449d62c1189ce0f6ae151c6a3
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2024-07-25 11:21:55 +0000

    a

--- routes/api.js
+++ routes/api.js
@@ -18,11 +18,17 @@ module.exports = function (app) {
     .post(function (req, res) {
       let project = req.params.project;
       const {issue_title, issue_text, created_by, assigned_to, status_text} = req.body
+
+      if ([issue_title, issue_text, created_by].includes(undefined)) {
+        return res.json({error: 'required field(s) missing'})
+      }
       
-      const issue = new Issue({issue_title, issue_text, created_by, assigned_to, status_text})
+      const issue = new Issue({issue_title, issue_text, created_by, assigned_to, status_text, project})
 
       issue.save((err, data) => {
-        if (err) return res.status(500).json({error: "error happened"})
+        if (err) return console.error(err)
+
+        delete data.project;
 
         return res.json(data)
       })
